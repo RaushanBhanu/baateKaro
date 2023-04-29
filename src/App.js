18c26345d2cd546f7e79cfc15975843f1663cc42
RaushanBhanu <roushan.bhanu123@gmail.com>
2023-04-29 17:39:55 +0530

    third

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Button from './components/BlueButton';
 import './styles/App.css';
 import './styles/common.css';
 import './styles/input.css'
+import './screens/Login'
+import Login from './screens/Login';
 
 function App() {
   const handleclick = () => {
@@ -10,7 +12,8 @@ function App() {
   return (
     <div className="App">
       {/* <h1 className=''>Hello</h1> */}
-      <Button text="Click Me" onClick={handleclick} />
+      {/* <Button text="Click Me" onClick={handleclick} /> */}
+      <Login/>
     </div>
   );
 }
--- src/components/BlueButton.js
+++ src/components/BlueButton.js
@@ -1,7 +1,7 @@
 const Button = ({text,onclick}) => {
     return (
         <div>
-            <button onclick={onclick}>
+            <button className="pl-36 pr-36 pt-3 pb-3 rounded-lg text-" onclick={onclick} style={{background:"var(--lBlueGradHori)"}}>
                 {text}
             </button>
         </div>
