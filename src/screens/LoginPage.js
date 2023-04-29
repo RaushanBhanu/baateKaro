7efd6408e55365546196c27ca8acbadce2c39474
RaushanBhanu <roushan.bhanu123@gmail.com>
2023-04-30 03:04:51 +0530

    eighth

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -6,6 +6,11 @@ const LoginPage = () => {
     const [email, setEmail] = useState("")
     const [pass, setPass] = useState("")
     const [username, setUser] = useState("")
+    const [isShown, setIsShown] = useState(false);
+    const handleClick = event => {
+        // toggle shown state
+        setIsShown(current => !current);
+    };
     return (
         <>
             <div className="h-[35rem]" style={{ background: "var(--lblueGrad)" }}>
@@ -17,17 +22,19 @@ const LoginPage = () => {
                         <div className="mb-16" id="logo" style={{ fontSize: "70px" }}>
                             Baate Karo
                         </div>
-                        <div className="mb-8 text-xl font-bold">
-                            LOGIN
-                        </div>
                         {
-                            true && <TextBox maxLength={20} outerClass={"mb-6"} width={"100%"} input={username} onChange={(e) => { setUser(e.target.value) }} placeholder="UserName" inputClass={"pl-4"} />
+                             <div className="mb-8 text-xl font-bold">
+                                {isShown?"Create Account":"Login"}
+                            </div>
+                        }
+                        {
+                            isShown && <TextBox maxLength={20} outerClass={"mb-6"} width={"100%"} input={username} onChange={(e) => { setUser(e.target.value) }} placeholder="UserName" inputClass={"pl-4"} />
 
                         }
                         <TextBox maxLength={50} outerClass={"mb-6"} width={"100%"} input={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" inputClass={"pl-4"} />
                         <TextBox maxLength={16} outerClass={"mb-10"} input={pass} width={"100%"} onChange={(e) => { setPass(e.target.value) }} placeholder="Password" inputClass={"pl-4"} type="password" />
                         <Button text={"Login"} />
-                        {/* <div className="mt-10 text-[13px]"><span className="opacity-50 mr-2">Don't have an account?</span><span onClick={} className="opacity-100 cursor-pointer">Create</span></div> */}
+                        <div className="mt-10 text-[13px]"><span className="opacity-50 mr-2">Don't have an account?</span><span onClick={handleClick} className="opacity-100 cursor-pointer">Create</span></div>
                     </div>
                 </div>
             </div>
